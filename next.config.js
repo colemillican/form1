e985d0e48a4c7ef103c40e5f70506fee7bb144f6
Cole Millican <info@streamflowintelligence.com>
2025-11-09 19:48:33 -0600

    Switch to CommonJS next.config.js and fix tracing root

---
+++ next.config.js
@@ -0,0 +1,12 @@
+﻿const path = require("path");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname),
+    appDir: true,
+  },
+};
+
+module.exports = nextConfig;
